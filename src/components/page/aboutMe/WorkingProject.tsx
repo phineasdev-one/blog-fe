d432885ede7c0ae03655a62cdd2101a7aad9816e
phianh.dev <phianh.dev@gmail.com>
2024-10-06 10:25:22 +0700

    First commit

--- src/components/page/aboutMe/WorkingProject.tsx
+++ src/components/page/aboutMe/WorkingProject.tsx
@@ -55,6 +55,18 @@ const WorkingProject = () => {
     },
   ];
 
+  const personalProject = [
+    {
+      projectName: "Doc collaboration",
+      img: 'phineasDocs.jpg',
+      role: t('page.aboutMe.project.symliv.role'),
+      description: "Ứng dụng cho phép nhiều người cùng chỉnh sửa tài liệu theo thời gian thực",
+      year: '2023',
+      url: 'https://docs.phineasdev.one/',
+    },
+
+  ];
+
   const handleRedirect = (url: string) => {
     if (!url) {
       setShowMessage(true);
@@ -90,6 +102,46 @@ const WorkingProject = () => {
                     alt="Phineas tran"
                     className="bdrd16 w-100"
                   /> */}
+                  <ImageWithSkeleton
+                    src={`../assets/imgs/page/about/${project.img}`}
+                    alt="Phineas tran"
+                    className="bdrd16 w-100"
+                  />
+                </div>
+                <div className="card-info">
+                  <h4 className="color-white mt-20">{project.projectName} </h4>
+                  <p className="h-10 color-white" style={{ height: '55px' }}>
+                    {project.description}
+                  </p>
+                  <div className="row align-items-center mt-25">
+                    <div className="col-7">
+                      <p className="color-gray-700 text-sm">{project.role}</p>
+                    </div>
+                    <div className="col-5 text-end">
+                      <span className="color-gray-700 text-sm timeread">
+                        {project.year}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <h3 className="color-white mb-30 wow animate__animated animate__fadeIn">
+        Các dự án cá nhân
+      </h3>
+      <div className="mt-50 mb-50">
+        <div className="row mt-50 mb-10">
+          {personalProject.map((project, index) => (
+            <div
+              className="col-lg-6 pointer"
+              key={index}
+              onClick={() => handleRedirect(project.url)}>
+              <div className="card-blog-1 hover-up wow animate__animated animate__fadeIn">
+                <div className="card-image mb-30 img-fluid">
                   <ImageWithSkeleton
                     src={`../assets/imgs/page/about/${project.img}`}
                     alt="Phineas tran"
